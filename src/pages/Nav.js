b5a2ba77daeae9e279bcf213b016a9507de1a1f7
gregorymark254 <98938152+gregorymark254@users.noreply.github.com>
2022-11-14 19:09:27 +0300

    Update Nav.js

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -14,23 +14,23 @@ const Nav = () => {
     <main className="nav">
       <div className="bg-black text-white">
         <div className="container mx-auto p-4">
-          <nav className='flex justify-between items-center  px-4'>
+          <nav className='flex justify-between items-center px-4'>
             {/* Top Nav */}
             <div className="flex items-center ">
               <img className="w-16 h-full" src={logo} alt="" />
             </div>
             <ul className='hidden md:flex'>
               <li>
-                <Link to="/" className="px-3 py-2 ">Home</Link>
+                <Link to="/" className="px-3 py-2 hover:text-orange-400">Home</Link>
               </li>
               <li>
-                <Link to="/about" className=" px-3 py-2 ">About Us</Link>
+                <Link to="/about" className=" px-3 py-2 hover:text-orange-400">About Us</Link>
               </li>
               <li>
-                <Link to="/gallary" className=" px-3 py-2 ">Gallary</Link>
+                <Link to="/gallary" className=" px-3 py-2 hover:text-orange-400">Gallery</Link>
               </li>
               <li>
-                <Link to="/contact" className="px-3 py-2">Contact Us</Link>
+                <Link to="/contact" className="px-3 py-2 hover:text-orange-400">Contact Us</Link>
               </li>
             </ul>
             <div className='hidden md:flex space-x-2'>
