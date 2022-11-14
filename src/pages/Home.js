f01d7ae7ac3156a006fbf76444fd5d57be6943a9
gregorymark254 <98938152+gregorymark254@users.noreply.github.com>
2022-11-14 19:33:57 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import 'tw-elements';
-import img2 from '../images/hero/cover1.jpg'
-import img1 from '../images/hero/cover2.jpg'
-import img3 from '../images/hero/cover3.JPG'
+import img2 from '../images/hero/COVER.jpg'
+import img1 from '../images/hero/COVER3.jpg'
+import img3 from '../images/hero/COVER2.jpg'
 import erick from '../images/client/erick.jpg'
 import sonia from '../images/client/sonia.jpg'
 import notiflow from '../images/client/notiflow.jpg'
