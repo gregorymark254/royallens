0fdb4bf4d22640a25279989e59d292d237d334be
gregorymark254 <98938152+gregorymark254@users.noreply.github.com>
2022-11-15 23:57:05 +0300

    Update Gallary.js

--- src/pages/Gallary.js
+++ src/pages/Gallary.js
@@ -1,16 +1,6 @@
 import React from 'react'
 import { BsArrowRight } from "react-icons/bs";
-import baby from '../images/babybump/cover.JPG'
-import birthday from '../images/babybump/cover.JPG'
-import commercial from '../images/commercial/commercial3.JPG'
-import couples from '../images/couples/cover.JPG'
-import events from '../images/events/cover.jpg'
-import grad from '../images/graduation/grad1.jpg'
-import person from '../images/personal/cover.jpg'
-import potrait from '../images/potrait/potrait.jpg'
-import street from '../images/street/street1.jpg'
-import studio from '../images/studio/cover.jpg'
-import wedding from '../images/wedding/wed.jpg'
+
 
 const Gallary = () => {
 
@@ -37,7 +27,7 @@ const Gallary = () => {
       <div className="container mx-auto p-4">
         <section id="feature">
           <div className="title-text">
-            <p><h1><b>My Potrfolio</b></h1></p>
+            <p className="text-xl font-bold"><b>My Potrfolio</b></p>
           </div>
         </section>
         <div className="text-center pb-6">
@@ -49,21 +39,21 @@ const Gallary = () => {
         <div className="md:grid grid-cols-3 gap-6 text-center client">
           <div className="hover:brightness-50 py-4">
             <a href="/potraits">
-              <img src={potrait} alt="portaits" width="400" height="300" />
+              <img src="https://i.postimg.cc/cLb6ht74/potrait.jpg" alt="portaits" loading="lazy" width="400" height="300" />
               <h2>Portaits</h2>
               <p className="flex justify-center items-center animate-bounce">The beauty of moments &nbsp;<BsArrowRight/></p>
             </a>
           </div>
           <div className="hover:brightness-50 py-4">
             <a href="/personal">
-              <img src={person} alt="portaits" width="400" height="300" />
+              <img src="https://i.postimg.cc/JhK82JJB/RLK-3084.jpg" alt="portaits" loading="lazy" width="400" height="300" />
               <h2>Personal Shoots</h2>
               <p className="flex justify-center items-center animate-bounce">Be yourself &nbsp;<BsArrowRight/></p>
             </a>
           </div>
           <div className="hover:brightness-50 py-4">
             <a href="/studio">
-              <img src={studio} alt="portaits" width="400" height="300" />
+              <img src="https://i.postimg.cc/j2jdbstS/studio37.jpg" alt="portaits" loading="lazy" width="400" height="300" />
               <h2>Studio Shot</h2>
               <p className="flex justify-center items-center animate-bounce">Capture your imaginations &nbsp;<BsArrowRight/></p>
             </a>
@@ -75,21 +65,21 @@ const Gallary = () => {
             <div className="md:grid grid-cols-3 gap-6 text-center client">
               <div className="hover:brightness-50 py-4">
                 <a href="/events">
-                  <img src={events} alt="portaits" width="400" height="300" />
+                  <img src="https://i.postimg.cc/wMXBSP9T/DSC-0236.jpg" alt="portaits" loading="lazy" width="400" height="300" />
                   <h2>Events</h2>
                   <p className="flex justify-center items-center animate-bounce">Creating the best day &nbsp;<BsArrowRight/></p>
                 </a>
               </div>
               <div className="hover:brightness-50 py-4">
                 <a href="/birthday">
-                  <img src="https://royalens.netlify.app/images/birth.jpg" alt="portaits" width="400" height="300" />
+                  <img src="https://i.postimg.cc/j5nq4xcB/birthday7.jpg" alt="portaits" loading="lazy" width="400" height="300" />
                   <h2>Birthday</h2>
                   <p className="flex justify-center items-center animate-bounce">It's your special day &nbsp;<BsArrowRight/></p>
                 </a>
               </div>
               <div className="hover:brightness-50 py-4">
                 <a href="/street">
-                  <img src={street} alt="portaits" width="400" height="300" />
+                  <img src="https://i.postimg.cc/76VF1cjv/DSC-4086.jpg" alt="portaits" loading="lazy" width="400" height="300" />
                   <h2>Street</h2>
                   <p className="flex justify-center items-center animate-bounce">Get out there and live a little &nbsp;<BsArrowRight/></p>
                 </a>
@@ -103,21 +93,21 @@ const Gallary = () => {
             <div className="md:grid grid-cols-3 gap-6 text-center client">
               <div className="hover:brightness-50 py-4">
                 <a href="/couples">
-                  <img src={couples} alt="portaits" width="400" height="300" />
+                  <img src="https://i.postimg.cc/NFG5b3MR/fashionv-4.jpg" alt="portaits" loading="lazy" width="400" height="300" />
                   <h2>Couples</h2>
                   <p className="flex justify-center items-center animate-bounce">All you need is love. &nbsp;<BsArrowRight/></p>
                 </a>
               </div>
               <div className="hover:brightness-50 py-4">
                 <a href="/baby-showers">
-                  <img src="https://royalens.netlify.app/images/shower1.jpg" alt="portaits" width="400" height="300" />
+                  <img src="https://royalens.netlify.app/images/shower1.jpg" alt="portaits" loading="lazy" width="400" height="300" />
                   <h2>Baby Showers</h2>
                   <p className="flex justify-center items-center animate-bounce">Dream big, little one &nbsp;<BsArrowRight/></p>
                 </a>
               </div>
               <div className="hover:brightness-50 py-4">
                 <a href="/baby">
-                  <img src={baby} alt="portaits" width="400" height="300" />
+                  <img src="https://i.postimg.cc/tTkmL6X8/Royallenske-1037.jpg" alt="portaits" loading="lazy" width="400" height="300" />
                   <h2>Baby Bump</h2>
                   <p className="flex justify-center items-center animate-bounce">The best is yet to come &nbsp;<BsArrowRight/></p>
                 </a>
@@ -131,21 +121,21 @@ const Gallary = () => {
             <div className="md:grid grid-cols-3 gap-6 text-center client">
               <div className="hover:brightness-50 py-4">
                 <a href="/graduation">
-                  <img src="https://royalens.netlify.app/images/grad1.jpg" alt="portaits" width="400" height="300" />
+                  <img src="https://i.postimg.cc/9MKNzFZf/eventsbb.jpg" alt="portaits" loading="lazy" width="400" height="300" />
                   <h2>Graduation</h2>
                   <p className="flex justify-center items-center animate-bounce">You did it,congatulations &nbsp;<BsArrowRight/></p>
                 </a>
               </div>
               <div className="hover:brightness-50 py-4">
                 <a href="/wedding">
-                  <img src={wedding} alt="portaits" width="400" height="300" />
+                  <img src="https://i.postimg.cc/xTRy418b/wed.jpg" alt="portaits" loading="lazy" width="400" height="300" />
                   <h2>Weddings</h2>
                   <p className="flex justify-center items-center animate-bounce">Best wishes! &nbsp;<BsArrowRight/></p>
                 </a>
               </div>
               <div className="hover:brightness-50 py-4">
                 <a href="/commercial">
-                  <img src={commercial} alt="portaits" width="400" height="300" />
+                  <img src="https://i.postimg.cc/K8hLWYH7/Royallenske-2780.jpg" alt="portaits" loading="lazy" width="400" height="300" />
                   <h2>Commercial</h2>
                   <p className="flex justify-center items-center animate-bounce">Add Value &nbsp;<BsArrowRight/></p>
                 </a>
