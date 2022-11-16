643854224dc29e99e05042efad903b1a8cf60f83
gregorymark254 <98938152+gregorymark254@users.noreply.github.com>
2022-11-16 10:50:27 +0300

    Update Wedding.js

--- src/pages/categories/Wedding.js
+++ src/pages/categories/Wedding.js
@@ -12,15 +12,12 @@ const Wedding = () => {
         <div className="grid lg:grid-cols-3 gap-6">
           <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover"
             data-mdb-ripple="true" data-mdb-ripple-color="dark">
-            <img src="https://royalens.netlify.app/images/wed1.jpg"
-              className="w-full transition duration-300 ease-linear align-middle" alt="" />
+            <img src="https://i.postimg.cc/pVYJtzKG/Royallenske-1771.jpg"
+              className="w-full transition duration-300 ease-linear align-middle" alt="" loading="lazy" />
             <div>
-              <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                >
-              </div>
               <div className="hover-overlay">
                 <div
-                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"
+                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out"
                   ></div>
               </div>
             </div>
@@ -28,15 +25,12 @@ const Wedding = () => {
 
           <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover"
             data-mdb-ripple="true" data-mdb-ripple-color="dark">
-            <img src="https://royalens.netlify.app/images/wed5.jpg"
-              className="w-full transition duration-300 ease-linear align-middle" alt="" />
+            <img src="https://i.postimg.cc/dt2Xph0w/Royallenske-1794.jpg"
+              className="w-full transition duration-300 ease-linear align-middle" alt="" loading="lazy" />
             <div>
-              <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                >
-              </div>
               <div className="hover-overlay">
                 <div
-                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"
+                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out"
                   ></div>
               </div>
             </div>
@@ -44,15 +38,12 @@ const Wedding = () => {
 
           <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover"
             data-mdb-ripple="true" data-mdb-ripple-color="dark">
-            <img src="https://royalens.netlify.app/images/wed8.jpg"
-              className="w-full transition duration-300 ease-linear align-middle" alt="" />
+            <img src="https://i.postimg.cc/hPssVTjz/Royallenske-1811.jpg"
+              className="w-full transition duration-300 ease-linear align-middle" alt="" loading="lazy" />
             <div>
-              <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                >
-              </div>
               <div className="hover-overlay">
                 <div
-                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"
+                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out"
                   ></div>
               </div>
             </div>
@@ -60,15 +51,12 @@ const Wedding = () => {
 
           <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover"
             data-mdb-ripple="true" data-mdb-ripple-color="dark">
-            <img src="https://royalens.netlify.app/images/wed10.jpg"
-              className="w-full transition duration-300 ease-linear align-middle" alt="" />
+            <img src="https://i.postimg.cc/CKV3db6Z/Royallenske-2280.jpg"
+              className="w-full transition duration-300 ease-linear align-middle" alt="" loading="lazy" />
             <div>
-              <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                >
-              </div>
               <div className="hover-overlay">
                 <div
-                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"
+                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out"
                   ></div>
               </div>
             </div>
@@ -76,15 +64,12 @@ const Wedding = () => {
 
           <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover"
             data-mdb-ripple="true" data-mdb-ripple-color="dark">
-            <img src="https://royalens.netlify.app/images/wed11.jpg"
-              className="w-full transition duration-300 ease-linear align-middle" alt="" />
+            <img src="https://i.postimg.cc/SsSHfk4k/Royallenske-2292.jpg"
+              className="w-full transition duration-300 ease-linear align-middle" alt="" loading="lazy" />
             <div>
-              <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                >
-              </div>
               <div className="hover-overlay">
                 <div
-                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"
+                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out"
                   ></div>
               </div>
             </div>
@@ -92,19 +77,57 @@ const Wedding = () => {
 
           <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover"
             data-mdb-ripple="true" data-mdb-ripple-color="dark">
-            <img src="https://royalens.netlify.app/images/wed13.jpg"
-              className="w-full transition duration-300 ease-linear align-middle" alt="" />
+            <img src="https://i.postimg.cc/rmq2Z516/Royallenske-2295.jpg"
+              className="w-full transition duration-300 ease-linear align-middle" alt="" loading="lazy" />
             <div>
-              <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                >
-              </div>
               <div className="hover-overlay">
                 <div
-                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"
+                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out"
                   ></div>
               </div>
             </div>
           </div>
+    
+           <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover"
+            data-mdb-ripple="true" data-mdb-ripple-color="dark">
+            <img src="https://i.postimg.cc/j5V3Mf7H/Royallenske-2266.jpg"
+              className="w-full transition duration-300 ease-linear align-middle" alt="" loading="lazy" />
+            <div>
+              <div className="hover-overlay">
+                <div
+                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out"
+                  ></div>
+              </div>
+            </div>
+          </div>
+    
+          <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover"
+            data-mdb-ripple="true" data-mdb-ripple-color="dark">
+            <img src="https://i.postimg.cc/qRXwQ50r/Royallenske-2251.jpg"
+              className="w-full transition duration-300 ease-linear align-middle" alt="" loading="lazy" />
+            <div>
+              <div className="hover-overlay">
+                <div
+                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out"
+                  ></div>
+              </div>
+            </div>
+          </div>
+    
+         <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover"
+            data-mdb-ripple="true" data-mdb-ripple-color="dark">
+            <img src="https://i.postimg.cc/J4CYNjLM/Royallenske-2445.jpg"
+              className="w-full transition duration-300 ease-linear align-middle" alt="" loading="lazy" />
+            <div>
+              <div className="hover-overlay">
+                <div
+                  className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out"
+                  ></div>
+              </div>
+            </div>
+          </div>
+    
+    
         </div>
       </div>
     </main>
